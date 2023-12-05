855c57d280cb712ce662cee0f209fb2e963c5a3c
yvoievid <yurii.voievidka@gmail.com>
2023-12-05 12:46:30 +0200

    added local battle simulateion skeleton

--- operation_simulation/models/unit.py
+++ operation_simulation/models/unit.py
@@ -1,6 +1,7 @@
 from gymnasium import spaces
 from dataclasses import dataclass,field
 from typing import List
+import numpy as np
 
 @dataclass
 class Unit:
@@ -10,12 +11,6 @@ class Unit:
     health: int = 25
     damage: int = 5
     speed: int = 1
-    # alpha: float = 0.1
-    # gamma: float = 0.6
-    # epsilon: float = 0.1
-    # lr: float = 0.1
-    # agent specific parameters
-    # up, down, left, right
     action_space: spaces.Space = spaces.Discrete(4)
 
     def draw(self):
